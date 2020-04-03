fb1e9311e5cfd29f005199ebb977acdb66638786
Mazhit <mig7676@yandex.ru>
2020-04-03 20:37:33 +0400

    Delete 6 filo Learning1

--- Lession 1-2.py
+++
@@ -1,7 +0,0 @@
-user_sec = int(input('Введите время в секундах: '))
-hour = user_sec//3600
-minute = user_sec%3600//60
-sec = user_sec%3600%60
-print(f'{"%.2d"%(hour)}:{"%.2d"%(minute)}:{"%.2d"%(sec)}')
-
-
--- Lession 1-3.py
+++
@@ -1,8 +0,0 @@
-count =input('Введите число n: ')
-count_n = count + count
-count_nn =count_n+count
-
-summa = int(count)+int(count_n)+int(count_nn)
-print(f' Сумма n+nn+nnn= {summa}')
-
-
